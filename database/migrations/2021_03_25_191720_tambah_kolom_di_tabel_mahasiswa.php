eb1459d43d7bca85171b704f3697e8f66c24adf9
vegaangga <vegaangga27@gmail.com>
2021-03-26 02:31:16 +0700

    Tugas No 1 Menambahkan 2 kolom -- Memodifikasi file migrasi agar dapat menambahkan 2 kolom baru email dan tgl_lahir

--- database/migrations/2021_03_25_191720_tambah_kolom_di_tabel_mahasiswa.php
+++ database/migrations/2021_03_25_191720_tambah_kolom_di_tabel_mahasiswa.php
@@ -13,7 +13,10 @@ class TambahKolomDiTabelMahasiswa extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('mahasiswa', function (Blueprint $table) {
+            $table->string('email',20)-> after('no_handphone')->nullable()->unique();
+            $table->date('tgl_lahir')-> after('email')->nullable()->unique();
+        });
     }
 
     /**
@@ -23,6 +26,9 @@ class TambahKolomDiTabelMahasiswa extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('mahasiswa', function (Blueprint $table) {
+            $table->dropColumn('email',20);
+            $table->dropColumn('tgl_lahir');
+        });
     }
 }
