07692f1cc3c4d05523d641f5bf3e6d838ac50423
vegaangga <vegaangga27@gmail.com>
2021-03-26 01:33:07 +0700

    Prak Langkah 8 -- Mengganti/mereplace default variabel model & menambahkan variable $fillable berdasarkan data yang ingin dapat kita tambah atau edit, pada kasus ini Nim, Nama, Kelas, Jurusan dan No_Handphone.

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
     use HasFactory;
+
+    protected $table='mahasiswa';
+    protected $primaryKey = 'nim';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas',
+        'jurusan',
+        'no_handphone'
+    ];
 }
