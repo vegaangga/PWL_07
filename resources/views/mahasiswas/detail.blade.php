bf366e875648b43b2090883d77298ee96e3116ff
vegaangga <vegaangga27@gmail.com>
2021-03-26 02:10:12 +0700

    Prak Langkah 9 Lanjutan -- Memodifikasi file layout, index, create, edit, detail. sehingga memunculkan halaman yang rapi

---
+++ resources/views/mahasiswas/detail.blade.php
@@ -0,0 +1,21 @@
+@extends('mahasiswas.layout')
+
+@section('content')
+<div class="container mt-5">
+   <div class="row justify-content-center align-items-center">
+      <div class="card" style="width: 24rem;">
+         <div class="card-header">Detail Mahasiswa</div>
+      <div class="card-body">
+         <ul class="list-group list-group-flush">
+         <li class="list-group-item"><b>Nim: </b>{{$mahasiswa->nim}}</li>
+         <li class="list-group-item"><b>Nama: </b>{{$mahasiswa->nama}}</li>
+         <li class="list-group-item"><b>Kelas: </b>{{$mahasiswa->kelas}}</li>
+         <li class="list-group-item"><b>Jurusan: </b>{{$mahasiswa->jurusan}}</li>
+         <li class="list-group-item"><b>No_Handphone: </b>{{$mahasiswa->no_handphone}}</li>
+         </ul>
+      </div>
+         <a class="btn btn-success mt3" href="{{ route('mahasiswa.index') }}">Kembali</a>
+      </div>
+    </div>
+</div>
+   @endsection
---
+++ resources/views/mahasiswas/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('mahasiswas.layout')
+
+@section('content')
+
+<div class="container mt-5">
+ <div class="row justify-content-center align-items-center">
+   <div class="card" style="width: 24rem;">
+      <div class="card-header">Edit Mahasiswa</div>
+      <div class="card-body">
+         @if ($errors->any())
+         <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+               @foreach ($errors->all() as $error)
+               <li>{{ $error }}</li>
+               @endforeach
+            </ul>
+         </div>
+         @endif
+         <form method="post" action="{{ route('mahasiswa.update', $mahasiswa->nim) }}" id="myForm">
+         @csrf
+         @method('PUT')
+            <div class="form-group">
+               <label for="Nim">Nim</label>
+               <br>
+               <input type="text" name="nim" class="form control" id="Nim" value="{{ $mahasiswa->nim }}" aria describedby="Nim" >
+            </div>
+            <div class="form-group">
+               <label for="Nama">Nama</label>
+               <br>
+               <input type="text" name="nama" class="form control" id="Nama" value="{{ $mahasiswa->nama }}" aria describedby="Nama" >
+            </div>
+            <div class="form-group">
+               <label for="Kelas">Kelas</label>
+               <br>
+               <input type="Kelas" name="kelas" class="form control" id="Kelas" value="{{ $mahasiswa->kelas }}" aria describedby="Kelas" >
+            </div>
+            <div class="form-group">
+               <label for="Jurusan">Jurusan</label>
+               <br>
+               <input type="Jurusan" name="jurusan" class="form control" id="Jurusan" value="{{ $mahasiswa->jurusan }}" aria describedby="Jurusan" >
+            </div>
+            <div class="form-group">
+               <label for="No_Handphone">No_Handphone</label>
+               <br>
+               <input type="No_Handphone" name="no_handphone" class="form control" id="No_Handphone" value="{{ $mahasiswa->no_handphone }}" aria describedby="No_Handphone" >
+            </div>
+               <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+         </div>
+      </div>
+    </div>
+</div>
+   @endsection
