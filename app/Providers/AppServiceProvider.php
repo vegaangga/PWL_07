bcca5103dc92747e684bc12065a7bbddedf16aab
vegaangga <vegaangga27@gmail.com>
2021-03-26 20:47:42 +0700

    Tugas No 2 Membuat paginate minimal 5 data -- Menambahkan fungsi kode paginator::useBootstrap pada AppServiceProvider.php agar fungsi paginate menerapkan bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use illuminate\Pagination\Paginator;
+//use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       Paginator::useBootstrap();
     }
 }
