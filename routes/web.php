957a5b9c01bc9e115ce5d7dd3e32a65776a3bee1
vegaangga <vegaangga27@gmail.com>
2021-03-26 21:49:32 +0700

    Tugas No 3 Menambahkan fitur search -- Menambahkan route /search pada file web.php

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('mahasiswa', MahasiswaController::class);
+
+Route::get('/search',[MahasiswaController::class, 'cari']);
