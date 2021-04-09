8b3526a4d2f341cd64667b0aece2dca8d1400f38
vegaangga <vegaangga27@gmail.com>
2021-04-09 10:07:16 +0700

    Prak 1 Langkah 6-7 Mengedit file KelasSeeder untuk menambahkan data awal pada table kelas

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KelasSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class KelasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $kelas = [
+            ['nama_kelas'=>'TI 2A'],
+            ['nama_kelas'=>'TI 2B'],
+            ['nama_kelas'=>'TI 2C'],
+            ['nama_kelas'=>'TI 2D'],
+            ['nama_kelas'=>'TI 2E'],
+            ['nama_kelas'=>'TI 2F'],
+            ['nama_kelas'=>'TI 2G'],
+            ['nama_kelas'=>'TI 2H'],
+            ['nama_kelas'=>'TI 2I']
+        ];
+
+        DB::table('kelas')->insert($kelas);
     }
 }
